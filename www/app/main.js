540aa1f61b0f8714cc6513e01d88243f918ceb74
hanson <hangzhong.yu@gmail.com>
2013-02-22 19:11:24 +0800

    init home voew

---
+++ www/app/main.js
@@ -0,0 +1,37 @@
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+//just for web dev
+//loadTemplates( appTemplatesLoaded );
+
+
+function onDeviceReady() {
+
+    //console.log( "READY" );
+
+    loadTemplates( appTemplatesLoaded );
+}
+
+function appTemplatesLoaded() {
+    //console.log( "VIEW TEMPLATES LOADED" );
+
+    $("body").empty();
+
+    var homeView = new HomeView();
+    
+    //Setup the ViewNavigator
+    window.viewNavigator = new ViewNavigator( 'body' );	
+    window.viewNavigator.pushView( homeView );
+    document.addEventListener("backbutton", onBackKey, false);
+}
+
+function onBackKey( event ) {
+    if ( window.viewNavigator.history.length > 1 ){
+        event.preventDefault();
+        window.viewNavigator.popView();
+        return false;
+    }
+    navigator.app.exitApp();
+}
+
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
