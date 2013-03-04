c67afeba2e7e1eb8f9e35dc52d139f0fca7eef07
hanson <hangzhong.yu@gmail.com>
2013-03-04 18:53:47 +0800

    fix bug of swipeview

--- www/libs/swipeview.js
+++ www/libs/swipeview.js
@@ -163,8 +163,8 @@ var SwipeView = (function (window, document) {
 
 		refreshSize: function () {
 			//this.wrapperWidth = this.wrapper.clientWidth;
-			this.wrapperWidth = this.wrapper.ownerDocument.body.clientWidth;
-			this.wrapperHeight = this.wrapper.ownerDocument.body.clientHeight;
+			this.wrapperWidth = this.wrapper.clientWidth==0 ? this.wrapper.ownerDocument.body.clientWidth : this.wrapper.clientWidth;
+			this.wrapperHeight = this.wrapper.clientHeight==0 ? this.wrapper.ownerDocument.body.clientHeight : this.wrapper.clientHeight;
 			//this.wrapperHeight = this.wrapper.clientHeight;
 			this.pageWidth = this.wrapperWidth;
 			this.maxX = -this.options.numberOfPages * this.pageWidth + this.wrapperWidth;
